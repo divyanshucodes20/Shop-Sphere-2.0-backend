88aebcc53135e49dd95224db18dab20757f40517
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-11-28 00:58:46 +0530

    Update CORS configuration to use dynamic client URL for improved flexibility

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [clientURL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
